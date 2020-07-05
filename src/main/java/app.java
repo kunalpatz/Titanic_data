3be2526cf54174a0a78880ab9fb481f4be6b1993
kunal <kunalpatil6495@gmail.com>
2020-07-05 17:05:30 +0200

    Updated and Formatting

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -9,6 +9,7 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
 import org.apache.spark.sql.types.DataTypes;
 
+
 public class app {
     public static void main(String[] args) {
         SparkSession app = SparkSession.builder()
@@ -17,17 +18,22 @@ public class app {
                 .appName("titanic").getOrCreate();
 
         Dataset<Row> df = app.read().format("csv").option("header", "true").option("inferSchema", "true")
-                .csv("src/TitanicInfo.csv").na().drop();;
+                .csv("src/dataset/TitanicInfo.csv").na().drop();
+
         app.sparkContext().setLogLevel("ERROR");
 
         df = df.withColumn("AgeGroup",
                 functions.when(functions.col("Age")
-                        .between(0,18), functions.lit("Child")).otherwise("Adult"));
+                        .between(0, 18), functions.lit("Child")).otherwise("Adult"));
+
+        df = df.withColumn("FamilySize", df.col("Siblings/Spouses Aboard")
+                .plus(df.col("Parents/Children Aboard")));
+
+        df = df.withColumn("Title", functions.split(functions.col("Name"), ". ").getItem(0));
+
+        df = df.drop("Parents/Children Aboard").drop("Siblings/Spouses Aboard").drop("Name");
 
 
-        // ##############################Survival##########################################################
-
-        // Survival Rate and Survival Percentage
 
         Dataset<Row> survivalRate = df.sort(functions.asc("Survived"))
                 .groupBy("Survived").agg(functions.count("*").alias("Count"));
@@ -36,110 +42,189 @@ public class app {
 
 
         Dataset<Row> survivalPercentage = df.groupBy("Survived")
-                .agg(functions.round(functions.count("*").divide(df.select("Survived").count()).multiply(100), 2).alias("Percentage"));
+                .agg(functions.round(functions.count("*")
+                        .divide(df.select("Survived")
+                        .count()).multiply(100), 2).alias("Percentage"));
         System.out.println("Total Survival Percentage");
         survivalPercentage.show();
 
-        // ########### By Gender
-        // Survival Rate and %
 
         Dataset<Row> survivalRateByGender = df.sort(functions.asc("Survived"))
                 .groupBy("Sex").agg(functions.count("*").alias("Count"));
-        System.out.println("Survival Rate by Gender");
+        System.out.println("Number of people on ship based on Gender");
         survivalRateByGender.show();
 
-        Dataset<Row> survivalByGenderPercentage = df.sort(functions.asc("Survived")).groupBy("Sex")
-                .agg(functions.round(functions.count("*").divide(df.select("Sex").count()).multiply(100), 2).alias("Percentage"));
-        System.out.println("Survival Percentage by Gender");
+
+        Dataset<Row> survivalByGenderPercentage = df.sort(functions.asc("Survived"))
+                .groupBy("Sex")
+                .agg(functions.round(functions.count("*")
+                        .divide(df.select("Sex")
+                        .count()).multiply(100), 2).alias("Percentage"));
+        System.out.println("Percentage of people on ship based on Gender");
         survivalByGenderPercentage.show();
 
 
         Dataset<Row> survivalRateByEachGender = df.sort(functions.asc("Survived"))
-                .groupBy("Survived","Sex").agg(functions.count("*").alias("Count")).orderBy("Sex");
-        System.out.println("Survival Rate for Each Gender");
+                .groupBy("Survived", "Sex")
+                .agg(functions.count("*").alias("Count")).orderBy("Sex");
+        System.out.println("Survival Rate by Gender");
         survivalRateByEachGender.show();
 
-        Dataset<Row> survivalByEachGenderPercentage = df.sort(functions.asc("Survived")).groupBy("Survived","Sex")
-                .agg(functions.round(functions.count("*").divide(df.select("Sex").count()).multiply(100), 2).alias("Percentage")).orderBy("Sex");
-        System.out.println("Survival Percentage for Each Gender");
+        Dataset<Row> survivalByEachGenderPercentage = df.sort(functions.asc("Survived"))
+                .groupBy("Survived", "Sex")
+                .agg(functions.round(functions.count("*").divide(df.select("Sex")
+                        .count()).multiply(100), 2).alias("Percentage")).orderBy("Sex");
+        System.out.println("Survival Percentage by Gender");
         survivalByEachGenderPercentage.show();
 
 
-
-        // ########### by Passenger Class
-        // Survival Rate and %
-
-        Dataset<Row> survivalRateByPClass = df.sort(functions.asc("Survived")).groupBy("Pclass").agg(functions.count("*"));
-        System.out.println("Survival Rate by Passenger Class");
+        Dataset<Row> survivalRateByPClass = df.sort(functions.asc("Survived"))
+                .groupBy("Pclass").agg(functions.count("*"));
+        System.out.println("Number of people on ship based on Passenger Class");
         survivalRateByPClass.show();
 
+
         Dataset<Row> survivalByPClassPercentage = df.groupBy("Pclass")
-                .agg(functions.round(functions.count("*").divide(df.select("Pclass").count()).multiply(100), 2).alias("Percentage"));
-        System.out.println("Survival Percentage by Passenger Class");
+                .agg(functions.round(functions.count("*")
+                        .divide(df.select("Pclass").count()).multiply(100), 2).alias("Percentage"));
+        System.out.println("Percentage of people on ship based on Passenger Class");
         survivalByPClassPercentage.show();
 
+
+        Dataset<Row> survivalRateByEachPClass = df.sort(functions.asc("Survived"))
+                .groupBy("Survived", "Pclass").agg(functions.count("*").alias("Count"))
+                .orderBy("Pclass");
+        System.out.println("Survival Rate by Passenger Class");
+        survivalRateByEachPClass.show();
+
+
+        Dataset<Row> survivalByEachPClassPercentage = df.sort(functions.asc("Survived"))
+                .groupBy("Survived", "Pclass")
+                .agg(functions.round(functions.count("*")
+                        .divide(df.select("Pclass").count()).multiply(100), 2)
+                        .alias("Percentage")).orderBy("Pclass");
+        System.out.println("Survival Percentage by Passenger Class");
+        survivalByEachPClassPercentage.show();
+
+
         Dataset<Row> survivalRateByPClassByGender = df.sort(functions.asc("Survived"))
-                .groupBy("Survived","Pclass", "Sex").agg(functions.count("*").alias("Count")).orderBy("Pclass");
-        System.out.println("Survival Rate for Each Class by Gender");
+                .groupBy("Survived", "Pclass", "Sex")
+                .agg(functions.count("*").alias("Count")).orderBy("Pclass");
+        System.out.println("Survival Rate for Passenger Class by Gender");
         survivalRateByPClassByGender.show();
 
+
         Dataset<Row> survivalByPClassByGenderPercentage = df.sort(functions.asc("Survived"))
-        .groupBy("Survived","Pclass","Sex")
-                .agg(functions.round(functions.count("*").divide(df.select("Pclass").count()).multiply(100), 2).alias("Percentage")).orderBy("Pclass");
-        System.out.println("Survival Percentage for Each Class by Gender");
+                .groupBy("Survived", "Pclass", "Sex")
+                .agg(functions.round(functions.count("*")
+                        .divide(df.select("Pclass").count()).multiply(100), 2)
+                        .alias("Percentage")).orderBy("Pclass");
+        System.out.println("Survival Percentage for Passenger Class by Gender");
         survivalByPClassByGenderPercentage.show();
 
-        // ########### By Age Group
-        // Survival Rate by Age Group
+
         Dataset<Row> survivalRateByAgeGroup = df.sort(functions.asc("Survived"))
                 .groupBy("AgeGroup").agg(functions.count("*").alias("Count"));
-
-        System.out.println("Survival Rate by Age");
+        System.out.println("Number of people on ship based on Age Group");
         survivalRateByAgeGroup.show();
 
-        // Survival % by Age Group
+
         Dataset<Row> survivalByAgeGroupPercentage = df.groupBy("AgeGroup")
                 .agg(functions.round(functions.count("*").divide(df.select("AgeGroup").count())
                         .multiply(100), 2).alias("Percentage"));
-        System.out.println("Survival Percentage by Age group");
+        System.out.println("Percentage of people on ship based on Age Group");
         survivalByAgeGroupPercentage.show();
 
 
+        Dataset<Row> survivalRateByEachAgeGroup = df.sort(functions.asc("Survived"))
+                .groupBy("Survived", "AgeGroup").agg(functions.count("*").alias("Count"));
+        System.out.println("Survival Rate by Age group");
+        survivalRateByEachAgeGroup.show();
+
+
+        Dataset<Row> survivalByEachAgeGroupPercentage = df.groupBy("Survived", "AgeGroup")
+                .agg(functions.round(functions.count("*")
+                        .divide(df.select("AgeGroup").count())
+                        .multiply(100), 2).alias("Percentage"));
+        System.out.println("Survival Percentage by Age group");
+        survivalByEachAgeGroupPercentage.show();
+
+
         Dataset<Row> survivalRateByGenderAgeGroup = df.sort(functions.asc("Survived"))
-                .groupBy("Survived","Sex","AgeGroup").agg(functions.count("*").alias("Count"));
-        System.out.println("Survival Rate by Gender and Age Group");
+                .groupBy("Survived", "Sex", "AgeGroup")
+                .agg(functions.count("*").alias("Count"));
+        System.out.println("Survival Rate for each Gender by Age Group");
         survivalRateByGenderAgeGroup.show();
+
 
         Dataset<Row> survivalByGenderAgeGroupPercentage = df.sort(functions.asc("Survived"))
                 .groupBy("Survived", "Sex", "AgeGroup")
-                .agg(functions.round(functions.count("*").divide(df.select("AgeGroup")
+                .agg(functions.round(functions.count("*")
+                        .divide(df.select("AgeGroup")
                         .count()).multiply(100), 2).alias("Percentage"));
-        System.out.println("Survival Percentage by Gender and Age Group");
+        System.out.println("Survival Percentage for each Gender by Age Group");
         survivalByGenderAgeGroupPercentage.show();
 
+
         Dataset<Row> survivalRateByPClassAgeGroup = df.sort(functions.asc("Survived"))
-                .groupBy("Survived","PClass","AgeGroup").agg(functions.count("*").alias("Count"));
+                .groupBy("Survived", "PClass", "AgeGroup")
+                .agg(functions.count("*").alias("Count"));
         System.out.println("Survival Rate by Passenger Class and Age Group");
         survivalRateByPClassAgeGroup.show();
 
+
         Dataset<Row> survivalByPClassAgeGroupPercentage = df.sort(functions.asc("Survived"))
                 .groupBy("Survived", "PClass", "AgeGroup")
-                .agg(functions.round(functions.count("*").divide(df.select("AgeGroup")
+                .agg(functions.round(functions.count("*")
+                        .divide(df.select("AgeGroup")
                         .count()).multiply(100), 2).alias("Percentage"));
         System.out.println("Survival Percentage by Passenger Class and Age Group");
         survivalByPClassAgeGroupPercentage.show();
 
 
+        Dataset<Row> survivalRateByTitle = df.sort(functions.asc("Survived"))
+                .groupBy("Title").agg(functions.count("*").alias("Count"));
+        System.out.println("Number of people on ship of different Titles");
+        survivalRateByTitle.show();
 
-        /*
+
+        Dataset<Row> survivalByTitlePercentage = df.groupBy("Title")
+                .agg(functions.round(functions.count("*")
+                        .divide(df.select("Title").count())
+                        .multiply(100), 2).alias("Percentage"));
+        System.out.println("Percentage of people on ship of different Titles");
+        survivalByTitlePercentage.show();
+
+
+        Dataset<Row> survivalRateByFare = df.sort(functions.asc("Survived"))
+                .groupBy("Survived")
+                .agg(functions.round(functions.sum(functions.col("Fare")), 3)
+                        .alias("Amount"));
+        System.out.println("Amount of Fare based on Survival Rate");
+        survivalRateByFare.show();
+
+
+        Dataset<Row> FarePerGender = df.sort(functions.asc("Sex"))
+                .groupBy("Sex")
+                .agg(functions.round(functions.sum(functions.col("Fare")), 3)
+                        .alias("Amount"));
+        System.out.println("Amount of Fare based on Gender");
+        FarePerGender.show();
+
+
+        Dataset<Row> FarePerClass = df.sort(functions.asc("PClass"))
+                .groupBy("PClass")
+                .agg(functions.round(functions.sum(functions.col("Fare")), 3).alias("Amount"));
+        System.out.println("Amount of Fare based on Passenger Class");
+        FarePerClass.show();
+
+
         StringIndexer sexIndexer = new StringIndexer();
         df = sexIndexer.setInputCol("Sex").setOutputCol("indexedSex").fit(df).transform(df);
 
-        //df.show();
-
         // Splitting DataSet into train and test models
 
-        Dataset<Row>[] trainTestSplit = df.randomSplit(new double[] { 0.8, 0.2 });
+        Dataset<Row>[] trainTestSplit = df.randomSplit(new double[]{0.8, 0.2});
 
         Dataset<Row> trainDf = trainTestSplit[0];
         Dataset<Row> testDf = trainTestSplit[1];
@@ -148,8 +233,8 @@ public class app {
 
         String featuresColumn = "features";
 
-        VectorAssembler vectorAssembler = new VectorAssembler().setInputCols(new String[] { "Pclass", "Age",
-                "Siblings/Spouses Aboard", "Parents/Children Aboard", "Fare", "indexedSex" })
+        VectorAssembler vectorAssembler = new VectorAssembler().setInputCols(new String[]{"Pclass", "Age",
+                "FamilySize", "Fare", "indexedSex"})
                 .setOutputCol(featuresColumn);
         trainDf = vectorAssembler.transform(trainDf);
         testDf = vectorAssembler.transform(testDf);
@@ -164,7 +249,7 @@ public class app {
 
         Dataset<Row> truthVsPrediction = predicted.select("Survived", "prediction");
 
-        //truthVsPrediction.show();
+        truthVsPrediction.show();
 
         // Renaming Survived column
         truthVsPrediction = truthVsPrediction
@@ -181,7 +266,6 @@ public class app {
             System.out.println(d);
         }
 
-         */
 
     }
 
